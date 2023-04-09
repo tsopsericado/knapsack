d8de65efec1393297b4573b68bc5090580948654
tsopsericado <ricardotsopse@gmail.com>
2023-04-09 19:24:53 +0100

    second commit

--- knapsack.js
+++ knapsack.js
@@ -30,6 +30,10 @@ function fillKnapsack(maxWeight, items) {
 
 //List of 20 different items that can be selected and put in the knapsack
 
-let items = [{name:"N",weight:x, value:y}]
+let items = [{name:"1", weight:2, value:6},
+    {name:"2", weight:5, value:3},
+    {name:"3", weight:4, value:1},{name:"4", weight:6, value:12},{name:"5", weight:1, value:4},{name:"6", weight:3, value:1},{name:"7", weight:9, value:2},{name:"8", weight:11, value:6},{name:"9", weight:7, value:1},{name:"10", weight:0, value:13},
+    {name:"11", weight:4, value:11},
+    {name:"12", weight:7, value:13},{name:"13", weight:5, value:3},{name:"14", weight:1, value:4},{name:"15", weight:9, value:5},{name:"16", weight:11, value:2},{name:"17", weight:16, value:2},{name:"18", weight:3, value:1},{name:"19", weight:1, value:13},{name:"20", weight:0, value:2}
+]
 
-items
