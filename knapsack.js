e08b65c7d7b348942f76a47ee1a232e9828251c8
tsopsericado <ricardotsopse@gmail.com>
2023-06-28 04:01:29 +0100

    some changes made in the js file

--- knapsack.js
+++ knapsack.js
@@ -1,76 +1,76 @@
-const itemList = document.querySelector('#item_list')
-const form = document.querySelector('form')
-const totalWeight = document.querySelector('#total_weigth')
-const totalValue = document.querySelector('#total_value')
-const maxWeight = document.querySelector('#max_weight')
+const itemList = document.querySelector("#item_list");
+const form = document.querySelector("form");
+const totalWeight = document.querySelector("#total_weight");
+const totalValue = document.querySelector("#total_value");
+const maxWeight = document.querySelector("#max_weight");
 const clearAllBtn = document.querySelector(".clear-all");
-const Add = document.querySelector('#add')
-const itemInput = document.querySelector('#item')
-const weightInput =document.querySelector('#weight')
-const valueInput = document.querySelector('#value')
+const Add = document.querySelector("#add");
+const itemInput = document.querySelector("#item");
+const weightInput = document.querySelector("#weight");
+const valueInput = document.querySelector("#value");
 const MaximumWeight = document.querySelector("#maxi_weight");
-const items = []
-form.addEvenListener('submit', sumItem)
+const items = [];
+form.addEvenListener("submit", sumItem);
 
-function sumItem (e) {
-    e.preventDefault()
+function sumItem(e) {
+  e.preventDefault();
 
-    const item = {
-       name: itemInput.value,
-       weight: parseInt(weightInput.value),
-       value: parseInt(valueInput.value),
-       maxW: parseInt(MaximumWeight.value)
-    }
-    
-    items.push(item)
+  const item = {
+    name: itemInput.value,
+    weight: parseInt(weightInput.value),
+    value: parseInt(valueInput.value),
+    maxW: parseInt(MaximumWeight.value),
+  };
 
-    calculateResult()
+  items.push(item);
 
-    itemInput.value = ''
-    weightInput.value = ''
-    valueInput.value = ''
-    MaximumWeight.disabled = true
+  calculateResult();
+
+  itemInput.value = "";
+  weightInput.value = "";
+  valueInput.value = "";
+  MaximumWeight.disabled = true;
 }
 
-function calculateResult () {
-   let totalWeightValue = 0
-   let totalValueValue = 0
-   let MaxWeigh = 0
-   let itemName
-   let weightInput
-   let valueInput
+function calculateResult() {
+  let totalWeightValue = 0;
+  let totalValueValue = 0;
+  let maxWeigh = 0;
+  let itemName;
+  let weightInput;
+  let valueInput;
 
-   items.forEach((item) =>{
-    totalWeightValue += item.weight
-    totalValueValue += item.value
-    MaxWeigh = item.maxW
-    itemName = item.name
-    weightInput = item.weight
-    valueInput = item.value
-   })
+  items.forEach((item) => {
+    totalWeightValue += item.weight;
+    totalValueValue += item.value;
+    maxWeigh = item.maxW;
+    itemName = item.name;
+    weightInput = item.weight;
+    valueInput = item.value;
+  });
 
-   if (totalWeightValue > MaxWeigh) {
-    window.alert(`the max weight allowed is ${MaxWeigh}`)
-    console.log(`The max weight allowed is ${MaxWeigh} and its full`)
-    Add.style.backgroundColor = 'orange'
-    Add.disabled = true
-   } else {
-    const li = document.createElement('li')
-    li.style.backgroundColor -'#afe1af'
+  if (totalWeightValue > MaxWeigh) {
+    window.alert(`the max weight allowed is ${maxWeigh}`);
+    console.log(`The max weight allowed is ${maxWeigh} and its full`);
+    Add.style.backgroundColor = "orange";
+    Add.disabled = true;
+  } else {
+    const li = document.createElement("li");
+    li.style.backgroundColor - "#afe1af";
     li.innerHTML = ` <span>${itemName}</span> <span>${weightInput}Kg</span> 
-    <span>${valueInput}XAF</span> `
+    <span>${valueInput}XAF</span> `;
 
     itemList.appendChild(li)
-    totalWeight.textContent = totalWeightValue
-    totalValue.textContent = totalValueValue
-    maxWeight.textContent = MaxWeigh;
-   }
+    totalWeight.textContent = totalWeightValue;
+    totalValue.textContent = totalValueValue;
+    maxWeight.textContent = maxWeigh;
+  }
 }
 
 clearAllBtn.onclick = function reset() {
-  window.location.reload()
-}
+  window.location.reload();
+};
 
-clearAllBtn.addEvenListener('click', () =>{
-  window.location.reload(true)
-})
+clearAllBtn.addEvenListener("click", () => {
+  window.location.reload(true);
+});
